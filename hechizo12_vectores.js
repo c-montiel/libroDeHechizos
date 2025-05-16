22ed7b1d2c46f6a1efa41e0b9b7e9e1d78632057
cmontiel <montielponcechristian@gmail.com>
2025-05-16 15:39:52 -0300

    actualizacion funcional de hechizo 12

--- hechizo12_vectores.js
+++ hechizo12_vectores.js
@@ -16,24 +16,56 @@ const INGREDIENTES_ENCONTRADOS = [];
 
 function main() {
     let ingredienteEncontrado = "";
-    
-    console.log("\n*** Iniciando recoleccion ***");
-    console.log("\n Qué ingredientes encontraste?: \n");
-    console.log(INGREDIENTE_01_FORMAT);
-    console.log(INGREDIENTE_02_FORMAT);
-    console.log(INGREDIENTE_03_FORMAT);
-    console.log(INGREDIENTE_04_FORMAT);
-    console.log(INGREDIENTE_05_FORMAT);
 
-    if (ingredienteEncontrado == 1 || ingredienteEncontrado == 2 || ingredienteEncontrado == 3) {
-        INGREDIENTES_ENCONTRADOS.push("leer()");
-    } Else {
-        console.log("_FORMAT");
-    }
+    for (let i = 0; i < 3; i++) {
 
-    
-console.log(INGREDIENTES_ENCONTRADOS);
+        console.log("\n*** Iniciando recoleccion ***");
+        console.log("\n Qué ingredientes encontraste?: \n");
+        console.log(INGREDIENTE_01_FORMAT);
+        console.log(INGREDIENTE_02_FORMAT);
+        console.log(INGREDIENTE_03_FORMAT);
+        console.log(INGREDIENTE_04_FORMAT);
+        console.log(INGREDIENTE_05_FORMAT);
 
+        ingredienteEncontrado = Number(leer());
+
+        switch (ingredienteEncontrado) {
+            case 1:
+                INGREDIENTES_ENCONTRADOS.push(INGREDIENTE_01);
+                console.log("\nIngrediente ACEPTADO!");
+                break;
+            case 2:
+                console.log("\nIngrediente NO ACEPTADO! Cuidado!");
+                break;
+            case 3:
+                INGREDIENTES_ENCONTRADOS.push(INGREDIENTE_03);
+                console.log("\nIngrediente ACEPTADO!");
+                break;
+            case 4:
+                INGREDIENTES_ENCONTRADOS.push(INGREDIENTE_04);
+                console.log("\nIngrediente ACEPTADO!");
+                break;
+            case 5:
+                console.log("\nIngrediente NO ACEPTADO! Cuidado!");
+                break;
+            default:
+                break;
+        };
+    };
+
+    console.log("\n\n*** FIN DE RECOLECCION ***");
+    console.log("\Informe de Ingredientes encontrados: ");
+
+    console.log("\n\t" + INGREDIENTES_ENCONTRADOS);
+    resultadoRecoleccion(INGREDIENTES_ENCONTRADOS)
 };
 
 main();
+
+function resultadoRecoleccion(INGREDIENTES_ENCONTRADOS) { 
+    if (INGREDIENTES_ENCONTRADOS.length >= 2) {
+        console.log("\nResultado: Aprobado");
+    } else {
+        console.log("\nResultado: Reprobado");
+    };
+    };
